c482f8f8040e0729a04a8a7f68bf3e229c18faaa
neal <nakfoury@gmail.com>
2020-12-01 22:35:23 -0800

    Solve day2

--- solutions/day2.py
+++ solutions/day2.py
@@ -1,2 +1,16 @@
+import re
+
+
 def day2(input, b=False):
-    return
+    result = 0
+    for pwd in input:
+        m = re.search(r'(\d+)-(\d+) (\w): (\w+)', pwd)
+        if m:
+            if not b:
+                if int(m.group(1)) <= m.group(4).count(m.group(3)) <= int(m.group(2)):
+                    result += 1
+            else:
+                if (m.group(4)[int(m.group(1)) - 1] == m.group(3) and m.group(4)[int(m.group(2)) - 1] != m.group(3)) or \
+                        (m.group(4)[int(m.group(1)) - 1] != m.group(3) and m.group(4)[int(m.group(2)) - 1] == m.group(3)):
+                    result += 1
+    return result
--- test_solutions.py
+++ test_solutions.py
@@ -4,3 +4,8 @@ from solutions import day1, day2
 def test_day1():
     assert day1.day1(['1721','979','366','299','675','1456']) == 514579
     assert day1.day1(['1721','979','366','299','675','1456'], True) == 241861950
+
+
+def test_day2():
+    assert day2.day2(['1-3 a: abcde','1-3 b: cdefg','2-9 c: ccccccccc']) == 2
+    assert day2.day2(['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc'], True) == 1
